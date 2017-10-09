993deab38793942fb56ea2cc4378a3b04c9a5375
Loris <loris.marini@sydney.edu.au>
2017-10-09 16:53:19 +1100

    Minor imrovements

--- Femto_Caching_30_Sept_DGPA_A/Network_A_W_D.m
+++ Femto_Caching_30_Sept_DGPA_A/Network_A_W_D.m
@@ -1,6 +1,42 @@
 function [ Net_A_W_D ] = Network_A_W_D( Network_Delays, S, Popularities, Allocation )
-%UNTITLED8 Summary of this function goes here
-%   Detailed explanation goes here
+%{
+-------------------------   AUTHORSHIP  -------------------------
+
+Developer: Loris Marini
+Affiliation: The University of Sydney
+Contact: mrnlrs.tor@gmail.com
+Notes:
+
+-------------------------   DESCRIPTION   -------------------------
+
+This function calculates the network average weighted delay as per eq(5) of
+Marini, L., Li, J., & Li, Y. (n.d.). Distributed Caching based on Decentralized Learning Automata, 1–6.
+
+------------------------- INPUT PARAMETERS -------------------------
+
+-- Network_Delays --
+Matrix of latencies [s] of size NxP with N = number of users, and P = number of 
+content providers. Row i-th corresponds to user i, and each column to the download 
+latency from corresponding provider (helper or base station).
+
+-- S -- 
+
+
+-- Popularities -- 
+
+-- Allocation --
+
+
+------------------------- OUTPUT PARAMETERS -------------------------
+
+-- Net_A_W_D -- 
+The average weighted delay of the entire network.
+
+------------------------- EXAMPLE OF CALL -----------------------
+
+
+% ----------------------------   CODE     --------------------------
+%}
 
 N = size(Network_Delays,1);
 Users_W_Ds = zeros(1,N);
