b1ec1d78a559f68f8024dbb04f326ad6507756f0
Loris <loris.marini@sydney.edu.au>
2017-10-10 14:43:27 +1100

    Migrates all applicable documentation to the October 4th directory which contains more up to date code

--- Femto_Caching_30_Sept_DGPA_A/shared_functions/Files_Popularities.m
+++
@@ -1,8 +0,0 @@
-function [ Popularities ] = Files_Popularities( S, Gamma_ZipF )
-
-% Gamma_ZipF: Determination of Popularities: ZIPF Distribution Law.
-
- Popularities = 1./(S.^Gamma_ZipF)...  % We assume that S corresponds to the action's ranking. Action 1 is then
-     ./(sum((1./S).^Gamma_ZipF));      % the most popular file. Action F is the least popular file.
-end
-
